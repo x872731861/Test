2eb5405c82af0e9e833cdb147f1bec4fb5971f52
x872731861 <x872731861@163.com>
2016-09-21 13:52:38 +0800

    Test

---
+++ childone/src/main/java/org/childone/App.java
@@ -0,0 +1,17 @@
+package org.childone;
+
+import org.childtwo.Person;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Person p=new Person("徐风雨",22,"江西九江");
+    	System.out.println(p);
+    	
+    }
+}
